ee519effc4cc184e99372d12a2227b76b90b55af
khariton <khariton.crp@mail.ru>
2025-07-24 13:33:12 +0300

    v 0.25

--- cmd/main.go
+++ cmd/main.go
@@ -3,34 +3,30 @@ package main
 import (
 	"log"
 
-	"github.com/4otis/vk-mini-app-cashflow-server/internal/config"
-	"github.com/4otis/vk-mini-app-cashflow-server/internal/handlers"
-	"github.com/4otis/vk-mini-app-cashflow-server/internal/models"
-	"github.com/4otis/vk-mini-app-cashflow-server/internal/repository"
-	"github.com/4otis/vk-mini-app-cashflow-server/internal/services"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// Инициализация базы данных
-	db, err := config.InitDB(config.Load().DB)
-	if err != nil {
-		log.Fatal("Database connection error:", err)
-	}
+	//db, err := config.InitDB(config.Load().DB)
+	//if err != nil {
+	//	log.Fatal("Database connection error:", err)
+	//}
 
 	// Автомиграции
-	if err := db.AutoMigrate(&models.Session{}, &models.Player{}); err != nil {
-		log.Fatal("Migration failed:", err)
-	}
+	//if err := db.AutoMigrate(&models.Session{}, &models.Player{}); err != nil {
+	//	log.Fatal("Migration failed:", err)
+	//}
 
 	// Инициализация зависимостей
-	sessionRepo := repository.NewSessionRepository(db)
-	playerRepo := repository.NewPlayerRepository(db)
-	sessionService := services.NewSessionService(sessionRepo, playerRepo)
-	sessionHandler := handlers.NewSessionHandler(sessionService)
+	//sessionRepo := repository.NewSessionRepository(db)
+	//playerRepo := repository.NewPlayerRepository(db)
+	//sessionService := services.NewSessionService(sessionRepo, playerRepo)
+	//sessionHandler := handlers.NewSessionHandler(sessionService)
 
-	_ = sessionHandler
+	//_ = sessionHandler
 	// // Создание роутера
-	// router := gin.Default()
+	router := gin.Default()
 
 	// router.StaticFile("/", "./index.html")           // Для корневого пути
 	// router.StaticFile("/index.html", "./index.html") // Явно для index.html
