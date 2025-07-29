d19e954f7dc18cb48e053e1c76644a159d6f8c12
khariton <khariton.crp@mail.ru>
2025-07-29 19:18:11 +0300

    Method was changed from UPDATE to PATHC. Ready option was removed from DTO-model.

--- internal/dto/game.go
+++ internal/dto/game.go
@@ -1,6 +1,6 @@
 package dto
 
 type PlayerIsReady struct {
-	VKID  int  `json:"vk_id" binding:"required"`
-	Ready bool `json:"ready" binding:"required"`
+	VKID int `json:"vk_id" binding:"required"`
+	// Ready bool `json:"ready" binding:"required"`
 }
