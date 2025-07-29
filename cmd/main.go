a5d401a4673bccb25c0e052a4df4acd9e765367f
khariton <khariton.crp@mail.ru>
2025-07-29 19:06:16 +0300

    RoutesSetup func was added.

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,10 @@ package main
 import (
 	"log"
 	"os"
-	"time"
 
 	"github.com/4otis/vk-mini-app-cashflow-server/internal/config"
 	"github.com/4otis/vk-mini-app-cashflow-server/internal/handlers"
 	"github.com/4otis/vk-mini-app-cashflow-server/internal/models"
-	"github.com/4otis/vk-mini-app-cashflow-server/internal/repository"
-	"github.com/4otis/vk-mini-app-cashflow-server/internal/services"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -34,37 +30,14 @@ func main() {
 		log.Fatal("Migration failed:", err)
 	}
 
-	// Инициализация зависимостей
-	sessionRepo := repository.NewSessionRepository(db)
-	playerRepo := repository.NewPlayerRepository(db)
-	sessionService := services.NewSessionService(sessionRepo, playerRepo)
-	sessionHandler := handlers.NewSessionHandler(sessionService)
+	g := gin.Default()
+	handlers.SetupRoutes(g, db)
 
-	// // Создание роутера
-	router := gin.Default()
-	_ = router
-
-	router.StaticFile("/", "./index.html")           // Для корневого пути
-	router.StaticFile("/index.html", "./index.html") // Явно для index.html
-
-	// Маршруты
-	router.POST("/sessions", sessionHandler.CreateSession)
-	router.POST("/sessions/:code/join", sessionHandler.JoinSession)
-	router.GET("/sessions/:code/players", sessionHandler.GetSessionPlayers)
-
-	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://cshflw.ru/*", "https://vk.com"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "UPDATE", "DELETE"},
-		AllowHeaders:     []string{"Origin", "Content-Type"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
 	// Запуск сервера
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 	log.Printf("Server running on port %s", port)
-	log.Fatal(router.Run(":" + port))
+	log.Fatal(g.Run(":" + port))
 }
---
+++ internal/handlers/routes.go
@@ -0,0 +1,41 @@
+package handlers
+
+import (
+	"time"
+
+	"github.com/4otis/vk-mini-app-cashflow-server/internal/repository"
+	"github.com/4otis/vk-mini-app-cashflow-server/internal/services"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func SetupRoutes(g *gin.Engine, db *gorm.DB) {
+	// Инициализация зависимостей
+	sessionRepo := repository.NewSessionRepository(db)
+	playerRepo := repository.NewPlayerRepository(db)
+
+	sessionService := services.NewSessionService(sessionRepo, playerRepo)
+	gameService := services.NewGameService(sessionRepo, playerRepo)
+
+	sessionHandler := NewSessionHandler(sessionService)
+	gameHandler := NewGameHandler(gameService)
+
+	g.StaticFile("/", "./index.html")           // Для корневого пути
+	g.StaticFile("/index.html", "./index.html") // Явно для index.html
+
+	// Маршруты
+	g.POST("/sessions", sessionHandler.CreateSession)
+	g.POST("/sessions/:code/join", sessionHandler.JoinSession)
+	g.GET("/sessions/:code/players", sessionHandler.GetSessionPlayers)
+	g.PATCH("/game/:code/ready", gameHandler.TryStartGame)
+
+	g.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://cshflw.ru/*", "https://vk.com"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+}
