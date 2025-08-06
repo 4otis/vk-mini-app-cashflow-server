3ccaa0c29d8ddcd93c4b7a72f2d4c3065b5db482
khariton <khariton.crp@mail.ru>
2025-08-06 14:05:37 +0300

    Showing card after rolledDice. v.0.31.

--- internal/dto/game.go
+++ internal/dto/game.go
@@ -39,7 +39,7 @@ type EndTurnReq struct {
 
 type Card struct {
 	Type   string     `json:"type"`
-	Asset  AssetCard  `json:"asset_card,omitempty"`
-	Market MarketCard `json:"market_card,omitempty"`
-	Issue  IssueCard  `json:"issue_card,omitempty"`
+	Asset  AssetCard  `json:"asset,omitempty"`
+	Market MarketCard `json:"market,omitempty"`
+	Issue  IssueCard  `json:"issue,omitempty"`
 }
