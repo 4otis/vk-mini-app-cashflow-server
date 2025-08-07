90bc0aaa2defe2d910b2a9c7f68e20323435e172
khariton <khariton.crp@mail.ru>
2025-08-07 21:13:51 +0300

    Trying to fix migrations v.0.2.

--- internal/models/asset.go
+++ internal/models/asset.go
@@ -4,10 +4,10 @@ import "gorm.io/gorm"
 
 type Asset struct {
 	gorm.Model
-	Title    string    `gorm:"size:50;not null" json:"title"`
-	Descr    string    `gorm:"size:100;not null" json:"descr"`
-	TypeID   int       `gorm:"index;not null" json:"type_id"`
-	Price    int       `gorm:"type:integer;default:0" json:"price"`
-	Cashflow int       `gorm:"type:integer;default:0" json:"cashflow"`
-	Players  []*Player `gorm:"many2many:players_assets;OnDelete:CASCADE"`
+	Title    string `gorm:"size:50;not null" json:"title"`
+	Descr    string `gorm:"size:100;not null" json:"descr"`
+	TypeID   int    `gorm:"index;not null" json:"type_id"`
+	Price    int    `gorm:"type:integer;default:0" json:"price"`
+	Cashflow int    `gorm:"type:integer;default:0" json:"cashflow"`
+	// Players  []*Player `gorm:"many2many:players_assets;OnDelete:CASCADE"`
 }
